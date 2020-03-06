6b7cd4f8693d4e74919b3fa030e5ffb110ffc095
zack256 <20821071+zack256@users.noreply.github.com>
2020-03-06 00:41:06 +0000

    renamed tables and classes.

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ import csv
 import random
 import math
 import start_db
+import manual_site as manual
 
 app = start_db.app; db = start_db.db
 
-class AutoActor (db.Model):
-    __tablename__ = "a_actors"
+class Actor (db.Model):
+    __tablename__ = "actors"
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(120))
     born = db.Column(db.Integer)
     died = db.Column(db.Integer)
     note = db.Column(db.String(1024))
     is_manual = db.Column(db.Boolean)
-    movies = db.relationship("AutoRole")
+    movies = db.relationship("Role")
 
     def format_name(self):
         if self.born:
@@ -31,28 +32,28 @@ class AutoActor (db.Model):
             return self.name + " (d." + str(self.died) + ")"
         return self.name
 
-class AutoMovie (db.Model):
-    __tablename__ = "a_movies"
+class Movie (db.Model):
+    __tablename__ = "movies"
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(120))
     year = db.Column(db.Integer)
     note = db.Column(db.String(1024))
     is_manual = db.Column(db.Boolean)
-    actors = db.relationship("AutoRole")
+    actors = db.relationship("Role")
 
     def format_title(self):
         if self.year:
             return self.title + " (" + str(self.year) + ")"
         return self.title
 
-class AutoRole (db.Model):
-    __tablename__ = "a_roles"
+class Role (db.Model):
+    __tablename__ = "roles"
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    actor_id = db.Column(db.Integer(), db.ForeignKey('a_actors.id'), primary_key = True, autoincrement = False)
-    movie_id = db.Column(db.Integer(), db.ForeignKey('a_movies.id'), primary_key = True, autoincrement = False)
+    actor_id = db.Column(db.Integer(), db.ForeignKey('actors.id'), primary_key = True, autoincrement = False)
+    movie_id = db.Column(db.Integer(), db.ForeignKey('movies.id'), primary_key = True, autoincrement = False)
     is_manual = db.Column(db.Boolean)
-    actor = db.relationship(AutoActor, backref = "a_movies_backref")
-    movie = db.relationship(AutoMovie, backref = "a_actors_backref")
+    actor = db.relationship(Actor, backref = "movies_backref")
+    movie = db.relationship(Movie, backref = "actors_backref")
 
 @app.route("/")
 def home_page_handler():
@@ -67,7 +68,7 @@ def get_asset_file(file_path):
 @app.route("/db/actors/<int:aid>")
 @app.route("/db/actors/<int:aid>/")
 def db_actor_pg_handle(aid):
-    actor = AutoActor.query.get(aid)
+    actor = Actor.query.get(aid)
     if actor == None:
         return "Actor not found!"
     movies = sorted([[role.movie, role.is_manual] for role in actor.movies], key = lambda x : [x[0].title])
@@ -82,7 +83,7 @@ def db_actor_pg_handle(aid):
 @app.route("/db/movies/<int:mid>")
 @app.route("/db/movies/<int:mid>/")
 def db_movie_pg_handle(mid):
-    movie = AutoMovie.query.get(mid)
+    movie = Movie.query.get(mid)
     if movie == None:
         return "Movie not found!"
     actors = sorted([[role.actor, role.is_manual] for role in movie.actors], key = lambda x : [x[0].name])
@@ -97,8 +98,8 @@ def db_movie_pg_handle(mid):
 @app.route("/db")
 @app.route("/db/")
 def db_home_pg_handle():
-    rand_actors = AutoActor.query.order_by(sql_func.random()).limit(config.constants.DB_HOME_ACTORS_SHOWN).all()
-    rand_movies = AutoMovie.query.order_by(sql_func.random()).limit(config.constants.DB_HOME_MOVIES_SHOWN).all()
+    rand_actors = Actor.query.order_by(sql_func.random()).limit(config.constants.DB_HOME_ACTORS_SHOWN).all()
+    rand_movies = Movie.query.order_by(sql_func.random()).limit(config.constants.DB_HOME_MOVIES_SHOWN).all()
     return render_template("main.html", actors = rand_actors, movies = rand_movies)
 
 @app.route("/db/actors")
@@ -106,7 +107,7 @@ def db_home_pg_handle():
 def db_actors_pg_handle():
     query = request.args.get("query", "", str)
     page = request.args.get("page", 1, int)
-    actor_query = AutoActor.query.filter(AutoActor.name.like(query + "%"))
+    actor_query = Actor.query.filter(Actor.name.like(query + "%"))
     total_matches = actor_query.count()
     if total_matches == 0:
         return render_template("actors.html", total_matches = total_matches, query = query, actors = [])
@@ -118,9 +119,9 @@ def db_actors_pg_handle():
         page = total_pages
     offset = (page - 1) * shown
     if query == "":
-        actors = actor_query.order_by(AutoActor.id).offset(offset).limit(shown).all()  # if no query is specified, sorts by ID. if this isn't happening then the actors shown will be ones with weird names with quotes at the front, not ideal.
+        actors = actor_query.order_by(Actor.id).offset(offset).limit(shown).all()  # if no query is specified, sorts by ID. if this isn't happening then the actors shown will be ones with weird names with quotes at the front, not ideal.
     else:
-        actors = actor_query.order_by(AutoActor.name).offset(offset).limit(shown).all()
+        actors = actor_query.order_by(Actor.name).offset(offset).limit(shown).all()
     return render_template("actors.html", actors = actors, total_matches = total_matches, page = page, total_pages = total_pages, query = query)
 
 @app.route("/db/movies")
@@ -128,7 +129,7 @@ def db_actors_pg_handle():
 def db_movies_pg_handle():
     query = request.args.get("query", "", str)
     page = request.args.get("page", 1, int)
-    movie_query = AutoMovie.query.filter(AutoMovie.title.like(query + "%"))
+    movie_query = Movie.query.filter(Movie.title.like(query + "%"))
     total_matches = movie_query.count()
     if total_matches == 0:
         return render_template("movies.html", total_matches = total_matches, query = query, movies = [])
@@ -140,14 +141,14 @@ def db_movies_pg_handle():
         page = total_pages
     offset = (page - 1) * shown
     if query == "":
-        movies = movie_query.order_by(AutoMovie.id).offset(offset).limit(shown).all()
+        movies = movie_query.order_by(Movie.id).offset(offset).limit(shown).all()
     else:
-        movies = movie_query.order_by(AutoMovie.title).offset(offset).limit(shown).all()
+        movies = movie_query.order_by(Movie.title).offset(offset).limit(shown).all()
     return render_template("movies.html", movies = movies, total_matches = total_matches, page = page, total_pages = total_pages, query = query)
 
 def remove_leading_spaces_from_all_entries():
     # single use, hopefully.
-    for actor in AutoActor.query.all():
+    for actor in Actor.query.all():
         actor.name = actor.name.strip()
     db.session.commit()
 
@@ -157,7 +158,7 @@ def db_add_actor_form_handle():
     born = request.form["born"]
     died = request.form["died"]
     note = request.form["note"]
-    actor = AutoActor()
+    actor = Actor()
     if name == "":
         return "Name can't be blank!"
     actor.name = name
@@ -176,7 +177,7 @@ def db_add_movie_form_handle():
     title = request.form["title"]
     year = request.form["year"]
     note = request.form["note"]
-    movie = AutoMovie()
+    movie = Movie()
     if title == "":
         return "Title can't be blank!"
     movie.title = title
@@ -201,7 +202,7 @@ def edit_a_year(given, former):
 @app.route("/db/forms/edit-actor/", methods = ["POST"])
 def db_edit_actor_form_handle():
     a_id = request.form["a_id"]
-    actor = AutoActor.query.get(int(a_id))
+    actor = Actor.query.get(int(a_id))
     if actor == None:
         return "Actor doesn't exist!"
     if actor.is_manual:
@@ -219,7 +220,7 @@ def db_edit_actor_form_handle():
 @app.route("/db/forms/edit-movie/", methods = ["POST"])
 def db_edit_movie_form_handle():
     m_id = request.form["m_id"]
-    movie = AutoMovie.query.get(int(m_id))
+    movie = Movie.query.get(int(m_id))
     if movie == None:
         return "Movie doesn't exist!"
     if movie.is_manual:
@@ -236,7 +237,7 @@ def db_edit_movie_form_handle():
 @app.route("/db/forms/delete-actor/", methods = ["POST"])
 def db_delete_actor_form_handle():
     a_id = request.form["a_id"]
-    actor = AutoActor.query.get(int(a_id))
+    actor = Actor.query.get(int(a_id))
     if actor == None:
         return "Actor doesn't exist!"
     if not actor.is_manual:
@@ -250,7 +251,7 @@ def db_delete_actor_form_handle():
 @app.route("/db/forms/delete-movie/", methods = ["POST"])
 def db_delete_movie_form_handle():
     m_id = request.form["m_id"]
-    movie = AutoMovie.query.get(int(m_id))
+    movie = Movie.query.get(int(m_id))
     if movie == None:
         return "Movie doesn't exist!"
     if not movie.is_manual:
@@ -264,7 +265,7 @@ def db_delete_movie_form_handle():
 def bridge_helper(l, home_dict, away_dict):
     c = l.pop(0)
     if c > 0:   # Actor
-        focus = AutoActor.query.get(c)
+        focus = Actor.query.get(c)
         for role in focus.movies:
             idx = -role.movie_id
             if idx in home_dict:
@@ -274,7 +275,7 @@ def bridge_helper(l, home_dict, away_dict):
             if idx in away_dict:
                 return idx
     else:   # Movie
-        focus = AutoMovie.query.get(-c)
+        focus = Movie.query.get(-c)
         for role in focus.actors:
             idx = role.actor_id
             if idx in home_dict:
@@ -331,23 +332,23 @@ def db_bacon_page():
         if case == 1:
             pass
         else:
-            a1 = AutoActor.query.get(a_id_1)
-            a2 = AutoActor.query.get(a_id_2)
+            a1 = Actor.query.get(a_id_1)
+            a2 = Actor.query.get(a_id_2)
             if a1 == None or a2 == None:
                 case = 1
             elif a1 == a2:
                 case = 2    # case 2 : same actors.
-                path_list = [AutoActor.query.get(a_id_1)]   # the actor is sent for jinja to display.
+                path_list = [Actor.query.get(a_id_1)]   # the actor is sent for jinja to display.
             else:
                 path_list = bridge_bfs(a_id_1, a_id_2)
                 if path_list:
                     case = 3    # case 3 : successful path found.
                     for path_node in path_list:
                         if path_node > 0:   # actor
-                            node = AutoActor.query.get(path_node)
+                            node = Actor.query.get(path_node)
                             path_dict[path_node] = node
                         else:   # movie
-                            node = AutoMovie.query.get(-path_node)
+                            node = Movie.query.get(-path_node)
                             path_dict[path_node] = node
                 else:
                     case = 4    # case 4 : no connection found- there are seperate groups of actors in the database.
@@ -361,7 +362,7 @@ def ajax_get_actor():
     if a_id == 0:
         fail = True
     else:
-        actor = AutoActor.query.get(a_id)
+        actor = Actor.query.get(a_id)
         if actor:
             res = actor.name
         else:
@@ -377,7 +378,7 @@ def ajax_get_movie():
     if m_id == 0:
         fail = True
     else:
-        movie = AutoMovie.query.get(m_id)
+        movie = Movie.query.get(m_id)
         if movie:
             res = movie.title
         else:
@@ -394,16 +395,16 @@ def db_add_role_form_handle():
         m_id = int(request.form["m_id"])
     except:
         fail = True
-    actor = AutoActor.query.get(int(a_id))
-    movie = AutoMovie.query.get(int(m_id))
+    actor = Actor.query.get(int(a_id))
+    movie = Movie.query.get(int(m_id))
     if actor == None or movie == None:
         fail = True
     if fail:
         return "Invalid ID(s)."
-    if AutoRole.query.filter((AutoRole.actor_id == a_id) & (AutoRole.movie_id == m_id)).first():
+    if Role.query.filter((Role.actor_id == a_id) & (Role.movie_id == m_id)).first():
         return "Role already exists!"
     coming_from = request.form["from"]
-    role = AutoRole()
+    role = Role()
     role.actor_id = a_id
     role.movie_id = m_id
     role.is_manual = True
@@ -421,7 +422,7 @@ def ajax_delete_role():
     if a_id == 0 or m_id == 0:
         fail = True
     else:
-        role = AutoRole.query.filter((AutoRole.actor_id == a_id) & (AutoRole.movie_id == m_id)).first()
+        role = Role.query.filter((Role.actor_id == a_id) & (Role.movie_id == m_id)).first()
         if role == None:
             fail = True
         else:
